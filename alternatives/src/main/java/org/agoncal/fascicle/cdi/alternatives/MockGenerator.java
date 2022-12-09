a7e832fc91c46ec0d43ac05bfcd31a6ef6ff1038
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-12-09 12:43:56 +0100

    Adoc

--- alternatives/src/main/java/org/agoncal/fascicle/cdi/alternatives/MockGenerator.java
+++ alternatives/src/main/java/org/agoncal/fascicle/cdi/alternatives/MockGenerator.java
@@ -1,15 +1,21 @@
 package org.agoncal.fascicle.cdi.alternatives;
 
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Default;
 
 /**
  * @author Antonio Goncalves
  * http://www.antoniogoncalves.org
  * --
  */
+@ApplicationScoped
 // tag::adocSnippet[]
 @Alternative
+// tag::adocSkip[]
+@Default
+// end::adocSkip[]
 public class MockGenerator implements NumberGenerator {
 
   public String generateNumber() {
